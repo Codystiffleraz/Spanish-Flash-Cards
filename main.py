d6ecb407e9e62d9ce2a521d495f80acca8f5819c
Cody Stiffler <codystiffleraz@gmail.com>
2023-11-06 15:33:38 -0700

    3 second timer then card flips to the english version

--- main.py
+++ main.py
@@ -2,28 +2,43 @@ from tkinter import *
 import random
 import pandas
 
+
 BACKGROUND_COLOR = "#B1DDC6"
 
 # ------PICKING WORDS  ------
 data = pandas.read_csv("data/Spanish_words.csv")
 # num = random.choice(range(0,100))
-
+current_card = {}
+    
 
 def next_card():
-    num = random.choice(range(0,100))
-    random_spanish_word = data["Spanish"][num]
-    canvas.itemconfig(card_title, text="Spanish")
-    canvas.itemconfig(card_word, text=random_spanish_word)
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(range(0,100))
+    random_spanish_word = data["Spanish"][current_card]
+    canvas.itemconfig(card_title, text="Spanish", fill="black")
+    canvas.itemconfig(card_word, text=random_spanish_word, fill="black")
+    canvas.itemconfig(card_background, image=flash_card_image)
+    flip_timer = window.after(3000, func=flip_card)
+    
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=data["English"][current_card], fill="white")
+    canvas.itemconfig(card_background, image=flash_card_image_back)
 
+    
 
 # ------ UI SETUP ------
 window = Tk()
 window.title("Flash Cards")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, func=flip_card)
+
 canvas = Canvas(width=800, height=526)
 flash_card_image = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=flash_card_image)
+flash_card_image_back = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=flash_card_image)
 card_title = canvas.create_text(400, 150, text="Title", fill="black", font=("Ariel", 40, "italic"))
 card_word = canvas.create_text(400, 263, text="word", fill="black", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
